086edfea319724d381f8d44ad2d95f854a2f1199
Prakash2403 <rishurai24@gmail.com>
2022-02-27 22:18:32 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import { DropdownGroup } from "./components/dropdown-group";
-export default function App() {
-  const dropdownValues = [
-    {
-      listValues: ["A", "B"],
-      label: "dropdown",
-      dropdownId: 1,
-      valueSetter: () => 1,
-    },
-    {
-      listValues: ["C", "D"],
-      label: "dropdown",
-      dropdownId: 2,
-      valueSetter: () => 1,
-    },
-    {
-      listValues: ["E", "F"],
-      label: "dropdown",
-      dropdownId: 3,
-      valueSetter: () => 1,
-    },
-  ];
+import AutocompleteDropDown from "./components/dropdown";
+import { useState } from "react";
+
+const people = [
+  { id: 1, name: "Durward Reynolds" },
+  { id: 2, name: "Kenton Towne" },
+  { id: 3, name: "Therese Wunsch" },
+  { id: 4, name: "Benedict Kessler" },
+  { id: 5, name: "Katelyn Rohan" },
+  { id: 10, name: "Durward Reynolds" },
+  { id: 20, name: "Kenton Towne" },
+  { id: 30, name: "Therese Wunsch" },
+  { id: 40, name: "Benedict Kessler" },
+  { id: 50, name: "Katelyn Rohan" },
+  { id: 100, name: "Durward Reynolds" },
+  { id: 200, name: "Kenton Towne" },
+  { id: 300, name: "Therese Wunsch" },
+  { id: 400, name: "Benedict Kessler" },
+  { id: 500, name: "Katelyn Rohan" },
+  { id: 1000, name: "Durward Reynolds" },
+  { id: 2000, name: "Kenton Towne" },
+  { id: 3000, name: "Therese Wunsch" },
+  { id: 4000, name: "Benedict Kessler" },
+  { id: 5000, name: "Katelyn Rohan" },
+];
+
+const App = () => {
+  const [selected_1, setSelected_1] = useState([]);
+  const [selected_2, setSelected_2] = useState([]);
+
   return (
-    <>
-      <DropdownGroup dropdownValues={dropdownValues} />
-    </>
+    <div className="max-w-md">
+      <AutocompleteDropDown
+        listValues={people}
+        displayProp="name"
+        label="People"
+        selected={selected_1}
+        setSelected={setSelected_1}
+      />
+      <AutocompleteDropDown
+        listValues={people}
+        displayProp="name"
+        label="People"
+        selected={selected_2}
+        setSelected={setSelected_2}
+      />
+    </div>
   );
-}
+};
+export default App;
---
+++ src/components/dropdown/useComponentVisible.js
@@ -0,0 +1,26 @@
+// Partially taken from https://stackoverflow.com/a/45323523/3865702
+
+import { useState, useEffect, useRef } from "react";
+
+export function useComponentVisible(initialIsVisible) {
+  const [isComponentVisible, setIsComponentVisible] =
+    useState(initialIsVisible);
+  const ref = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setIsComponentVisible(false);
+    } else {
+      setIsComponentVisible(true);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, []);
+
+  return { ref, isComponentVisible, setIsComponentVisible };
+}
